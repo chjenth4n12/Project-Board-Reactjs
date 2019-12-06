0ee1f64ceaa0c6974fee91e49d7bcba9873c5384
Nguyễn Xuân Tùng <nguyenxuantungk37@gmail.com>
2019-12-06 08:56:03 +0700

    update
    + Routing
    + Redux store
    + Controller Form

--- src/component/ProjectBoard.js
+++ src/component/ProjectBoard.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import ProjectTaskItem from './ProjectTask/ProjectTaskItem';
 
 class ProjectBoard extends Component {
     render() {
         return (
             <div className="container">
-                <a href="" className="btn btn-primary mb-3">
+                <Link to="/addProjectTask" className="btn btn-primary mb-3">
                     <i className="fas fa-plus-circle"> Create Project Task</i>
-                </a>
+                </Link>
                 <br />
                 <hr />
                 {/*Backlog STARTS HERE*/}
@@ -20,25 +22,8 @@ class ProjectBoard extends Component {
                             </div>
 
                             {/*SAMPLE PROJECT TASK STARTS HERE*/}
-                            <div className="card mb-1 bg-light">
-
-                                <div className="card-header text-primary">
-                                    ID: projectSequence
-                                </div>
-                                <div className="card-body bg-light">
-                                    <h5 className="card-title">summary</h5>
-                                    <p className="card-text text-truncate ">
-                                        acceptanceCriteria
-                                    </p>
-                                    <a href="" className="btn btn-primary">
-                                        View / Update
-                                    </a>
-
-                                    <button className="btn btn-danger ml-4">
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
+                            
+                            <ProjectTaskItem></ProjectTaskItem>
 
                             {/*SAMPLE PROJECT TASK ENDS HERE*/}
                         </div>
@@ -50,6 +35,8 @@ class ProjectBoard extends Component {
                             </div>
                             {/* SAMPLE PROJECT TASK STARTS HERE */}
 
+                            <ProjectTaskItem></ProjectTaskItem>
+                            
                             {/* SAMPLE PROJECT TASK ENDS HERE */}
                         </div>
                         <div className="col-md-4">
