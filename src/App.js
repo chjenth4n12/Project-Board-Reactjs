17b4f868f79302dc844e49469106ba50f6b88755
Nguyễn Xuân Tùng <nguyenxuantungk37@gmail.com>
2019-12-06 11:55:43 +0700

    update
    Redux Store

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import ProjectBoard from './component/ProjectBoard';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProjectTask from './component/ProjectTask/AddProjectTask';
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
     render () {
         return (
-            <Router>
-                <div className="App">
-                    <Navbar></Navbar>
-                    <Route exact path="/" component={ProjectBoard} />
-                    <Route exact path="/addProjectTask" component={AddProjectTask} />
-                </div>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar></Navbar>
+                        <Route exact path="/" component={ProjectBoard} />
+                        <Route exact path="/addProjectTask" component={AddProjectTask} />
+                    </div>
+                </Router>
+            </Provider>
         );
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import errorsReducers from "./errorsReducers";
 
 export default combineReducers ({
     //
+    errors: errorsReducers
 });
