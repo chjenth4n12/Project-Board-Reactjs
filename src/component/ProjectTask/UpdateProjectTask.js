16253f260abff9e339f288b361b700773970b24b
Nguyễn Xuân Tùng <nguyenxuantungk37@gmail.com>
2019-12-12 15:46:11 +0700

    update project task final

--- src/component/ProjectTask/UpdateProjectTask.js
+++ src/component/ProjectTask/UpdateProjectTask.js
@@ -3,15 +3,63 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
-import { getProjectTask } from "../../actions/ProjectTaskActions";
+import { getProjectTask, addProjectTask } from "../../actions/ProjectTaskActions";
 
 class UpdateProjectTask extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: "",
+            summary: "",
+            acceptanceCriteria: "",
+            status: "",
+            errors: {}
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+
+        const { id, summary, acceptanceCriteria, status } = nextProps.project_task;
+        this.setState({
+            id,
+            summary,
+            acceptanceCriteria,
+            status
+        });
+    }
 
     componentDidMount() {
         const {id} = this.props.match.params;
         this.props.getProjectTask(id);
     }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const updateTaskItem = {
+            id: this.state.id,
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status
+        };
+        
+        this.props.addProjectTask(updateTaskItem, this.props.history);
+    }
+
     render() {
+        const { errors } = this.state;
         return (
             <div className="addProjectTask">
                 <div className="container">
@@ -21,15 +69,16 @@ class UpdateProjectTask extends Component {
                                 Back to Board
                             </a>
                             <h4 className="display-4 text-center">Add /Update Project Task</h4>
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                                    <input type="text" className={classnames("form-control form-control-lg", { "is-invalid": errors.summary })} name="summary" value={this.state.summary} onChange={this.onChange} placeholder="Project Task summary" />
+                                    {errors.summary && ( <div className="invalid-feedback">{errors.summary}</div> )}
                                 </div>
                                 <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                                    <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" value={this.state.acceptanceCriteria} onChange={this.onChange} name="acceptanceCriteria"></textarea>
                                 </div>
                                 <div className="form-group">
-                                    <select className="form-control form-control-lg" name="status">
+                                    <select className="form-control form-control-lg" name="status" value={this.state.status} onChange={this.onChange} >
                                         <option value="">Select Status</option>
                                         <option value="TO_DO">TO DO</option>
                                         <option value="IN_PROGRESS">IN PROGRESS</option>
@@ -49,12 +98,13 @@ class UpdateProjectTask extends Component {
 UpdateProjectTask.propTypes = {
     project_task: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired,
-    getProjectTask: PropTypes.func.isRequired
+    getProjectTask: PropTypes.func.isRequired,
+    addProjectTask: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-    project_task: state.project_task,
+    project_task: state.project_task.project_task,
     errors: state.errors
 });
 
-export default connect (mapStateToProps, { getProjectTask }) (UpdateProjectTask);
+export default connect (mapStateToProps, { getProjectTask, addProjectTask }) (UpdateProjectTask);
